b5b8ab771099f544bf7e80c98f4e1d36707cf451
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-12-10 23:49:04 +0100

    implement start and polling of the bot

--- src/commands.py
+++ src/commands.py
@@ -14,5 +14,27 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     stored_language = get_set_language_choice(1);
 
+    if stored_language == "English":
+        await update.message.reply_text("Give me you prompt, what do you want to save?\n\nExample: Today I spent 2000FCFA on transport.");
+    else:
+        await update.message.reply_text("Donnez-moi une idée de ce que vous voulez économiser.\n\nExemple : Aujourd'hui, j'ai dépensé 2000FCFA pour le transport.")
+
+
+async def language_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Select your preferred language: \n\nSélectionnez votre langue préférée :");
+
+    # prompt to ask to select language
+    selected_language = get_language_keyboard();
+
+    while selected_language is not "English" or selected_language is not "French":
+        selected_language = get_language_keyboard();
+    
+    stored_language = get_set_language_choice(1);
+
+    if stored_language == "English":
+        await update.message.reply_text("Give me you prompt, what do you want to save?\n\nExample: Today I spent 2000FCFA on transport.");
+    else:
+        await update.message.reply_text("Donnez-moi une idée de ce que vous voulez économiser.\n\nExemple : Aujourd'hui, j'ai dépensé 2000FCFA pour le transport.")
+
 
 
--- src/main.py
+++ src/main.py
@@ -2,9 +2,15 @@ import os
 from dotenv import load_dotenv
 from bardapi import Bard
 import sqlite3
+from telegram import Update
+from telegram.ext import Application, Updater, CommandHandler, MessageHandler, filters, CallbackContext, CallbackQueryHandler, ContextTypes
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from commands import start_command, language_command
+from responses import handle_message, handle_response, error
 
 load_dotenv()  # This line brings all environment variables from .env into os.environ
 BARD_API_KEY = os.environ.get("BARD_API_KEY");
+TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN");
 BOT_USERNAME = os.environ.get("BOT_USERNAME");
 
 # Connect to database
@@ -23,3 +29,21 @@ connection.commit();
 
 
 bard = Bard(token=BARD_API_KEY, language = "English")
+
+if __name__ == "__main__":
+    print("Starting bot...")
+    app = Application.builder().token(TELEGRAM_BOT_TOKEN).build();
+
+    # Commands
+    app.add_handler(CommandHandler('start', start_command));
+    app.add_handler(CommandHandler('language', language_command));
+
+    # Messages responses
+    app.add_handler(MessageHandler(filters.TEXT, handle_message));
+
+    # Errors
+    app.add_error_handler(error);
+
+    print("Polling...")
+    # Polls the bot
+    app.run_polling(poll_interval=3)
